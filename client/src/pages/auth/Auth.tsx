05a541e23a01d894e299e217b503ed2a77793462
Dawaman43 <dawitworkujima@gmail.com>
2025-09-25 12:31:46 +0300

    added auth.tsx page

--- client/src/pages/auth/Auth.tsx
+++ client/src/pages/auth/Auth.tsx
@@ -1,5 +1,26 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function AuthPage() {
-  return <div>auth page</div>;
+  return (
+    <div className="flex w-full max-w-sm flex-col gap-6">
+      <Tabs defaultValue="login" className="w-full">
+        <TabsList>
+          <TabsTrigger value="login" className="w-1/2">
+            Login
+          </TabsTrigger>
+          <TabsTrigger value="register" className="w-1/2">
+            Register
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          <div>Login form goes here</div>
+        </TabsContent>
+        <TabsContent value="register">
+          <div>Register form goes here</div>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
 
 export default AuthPage;
