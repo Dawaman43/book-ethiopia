fdb5883471b66065290addb50c7c2be6b2847e5a
Dawaman43 <dawitworkujima@gmail.com>
2025-09-25 12:33:38 +0300

    added login and register component

---
+++ client/src/components/auth/login.tsx
@@ -0,0 +1,5 @@
+function Login() {
+  return <div>Login form goes here</div>;
+}
+
+export default Login;
---
+++ client/src/components/auth/register.tsx
@@ -0,0 +1,5 @@
+function Register() {
+  return <div>Register form goes here</div>;
+}
+
+export default Register;
