f9a477d4ff754c74375ccbbf2b4ef4059ebf795d
Dawaman43 <dawitworkujima@gmail.com>
2025-09-26 10:50:00 +0300

    added review model

---
+++ server/models/review.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    hotel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+    rating: { type: Number, min: 1, max: 5, required: true },
+    comment: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Review", reviewSchema);
